f7a36737800c5c8f71c944199c8960f00e547286
Riku <riku.toivanen123@gmail.com>
2025-10-02 19:54:10 +0300

    added functions

--- my-app/src/main/java/com/example/TemperatureConverter.java
+++ my-app/src/main/java/com/example/TemperatureConverter.java
@@ -9,6 +9,10 @@ public class TemperatureConverter {
         return (celsius * 9 / 5) + 32;
     }
 
+    public static double celsiusToKelvin(double celsius) {
+        return celsius + 273.15;
+    }
+
     public static boolean isExtremeTemperature(double celsius) {
         return celsius < -40 || celsius > 50;
     }
